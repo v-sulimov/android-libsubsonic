d3e0e175e971fe2bd71b23e88b4cc403f7038f03
Vitaly Sulimov <v.sulimov.dev@imap.cc>
2026-01-02 15:17:27 +0300

    Create library project
    Initialized the project using the following template: https://git.vsulimov.com/android-library-template.git

---
+++ libsubsonic/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
+}
+
+android {
+    namespace = "com.vsulimov.libsubsonic"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        lint.targetSdk = 36
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
+}
+
+dependencies {
+}
+
+publishing {
+    publications {
+        afterEvaluate {
+            create<MavenPublication>("release") {
+                groupId = "com.vsulimov"
+                artifactId = "libsubsonic"
+                version = "1.0.0"
+
+                from(components["release"])
+
+                pom {
+                    packaging = "aar"
+                    name = "Subsonic"
+                    description = "A lightweight Android library for interacting with the Subsonic API."
+                    licenses {
+                        license {
+                            name = "The MIT License (MIT)"
+                            url = "https://mit-license.org/"
+                        }
+                    }
+                    developers {
+                        developer {
+                            name = "Vitaly Sulimov"
+                            email = "v.sulimov.dev@imap.cc"
+                        }
+                    }
+                    scm {
+                        url = "https://git.vsulimov.com/android-libsubsonic.git"
+                    }
+                }
+            }
+        }
+    }
+}
